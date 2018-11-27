826739f96a4dcac158d3deb098979eab39e07be5
Martyn Jones <martynj2010@icloud.com>
2018-11-27 12:31:27 +0000

    Initial Commit

---
+++ fib.rb
@@ -0,0 +1,20 @@
+# Fibonacci Function - The Odin Project Ruby recursion lessons.
+
+def fibs (n) #Non-recursive
+
+	# n = number of Fibonacci sequence members.
+	# 0, 1, 1, 2, 3, 5, 8, 13, ..	
+	fib_seq[]
+	(0..n).each |i|
+		if i == 0
+			fib_no << 0
+		else
+			fib_no << (i + (i - 1))
+		end
+	end
+	fib_seq
+end
+
+def fibs_rec #Recursive	
+
+end
